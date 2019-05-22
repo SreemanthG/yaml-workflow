7fb97c85947d659a8cc1402398de4ef4324ba198
Thomas Hallgren <thomas@tada.se>
2019-05-22 17:27:59 +0200

    Fix failing test
    Fixes a failing test that was fixed once before, but in the
    puppet-workflow module.

--- yaml/activity_test.go
+++ yaml/activity_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/lyraproj/pcore/pcore"
@@ -182,7 +183,7 @@ func ExampleCreateStep() {
 func TestParse_unresolvedType(t *testing.T) {
 	requireError(t, `Reference to unresolved type 'No::Such::Type' (file: testdata/typefail.yaml, line: 3, column: 5)`, func() {
 		pcore.Do(func(ctx px.Context) {
-			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "testdata", ``, px.PuppetDataTypePath))
+			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "./testdata", ``, px.PuppetDataTypePath))
 			workflowFile := "testdata/typefail.yaml"
 			content, err := ioutil.ReadFile(workflowFile)
 			if err != nil {
@@ -196,7 +197,7 @@ func TestParse_unresolvedType(t *testing.T) {
 func TestParse_unparsableType(t *testing.T) {
 	requireError(t, `expected one of ',' or '}', got '' (file: testdata/typeparsefail.yaml, line: 6, column: 11)`, func() {
 		pcore.Do(func(ctx px.Context) {
-			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "testdata", ``, px.PuppetDataTypePath))
+			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "./testdata", ``, px.PuppetDataTypePath))
 			workflowFile := "testdata/typeparsefail.yaml"
 			content, err := ioutil.ReadFile(workflowFile)
 			if err != nil {
@@ -209,10 +210,10 @@ func TestParse_unparsableType(t *testing.T) {
 
 func TestParse_mismatchedType(t *testing.T) {
 	requireError(t,
-		"error while building call typemismatchfail (file: testdata/typemismatchfail.yaml, line: 11, column: 7)\nCaused by: invalid arguments for function Integer: expects one of:\n  (Convertible 1, Radix 2, Boolean 3)\n    rejected: parameter 1 variant '0' expects a Numeric value, got String\n    rejected: parameter 1 variant '1' expects a Boolean value, got String\n    rejected: parameter 1 variant '2' expects a match for Pattern[/\\\\A[+-]?\\\\s*(?:(?:0|[1-9]\\\\d*)|(?:0[xX][0-9A-Fa-f]+)|(?:0[0-7]+)|(?:0[bB][01]+))\\\\z/], got 'three // Not a number'\n    rejected: parameter 1 variant '3' expects a Timespan value, got String\n    rejected: parameter 1 variant '4' expects a Timestamp value, got String\n  (NamedArgs 1)\n    rejected: parameter 1 expects a NamedArgs value, got String (file: /home/thhal/go/pkg/mod/github.com/lyraproj/pcore@v0.0.0-20190516164225-2c1838ece043/pximpl/function.go, line: 316)",
+		regexp.MustCompile(`(?m:/typemismatchfail.yaml, line: 11, column: 7\)\s*Caused by: invalid arguments for function Integer)`),
 		func() {
 			pcore.Do(func(ctx px.Context) {
-				ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "testdata", ``, px.PuppetDataTypePath))
+				ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "./testdata", ``, px.PuppetDataTypePath))
 				workflowFile := "testdata/typemismatchfail.yaml"
 				content, err := ioutil.ReadFile(workflowFile)
 				if err != nil {
@@ -226,7 +227,7 @@ func TestParse_mismatchedType(t *testing.T) {
 func TestParse_unresolvedAttr(t *testing.T) {
 	requireError(t, `A Kubernetes::Namespace has no attribute named no_such_attribute (file: testdata/attrfail.yaml, line: 3, column: 14)`, func() {
 		pcore.Do(func(ctx px.Context) {
-			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "testdata", ``, px.PuppetDataTypePath))
+			ctx.SetLoader(px.NewFileBasedLoader(ctx.Loader(), "./testdata", ``, px.PuppetDataTypePath))
 			workflowFile := "testdata/attrfail.yaml"
 			content, err := ioutil.ReadFile(workflowFile)
 			if err != nil {
@@ -235,6 +236,26 @@ func TestParse_unresolvedAttr(t *testing.T) {
 			yaml.CreateStep(ctx, workflowFile, content)
 		})
 	})
+}
+
+func requireError(t *testing.T, msg interface{}, f func()) {
+	t.Helper()
+	defer func() {
+		t.Helper()
+		if r := recover(); r != nil {
+			if err, ok := r.(error); ok {
+				if s, ok := msg.(string); ok {
+					require.Equal(t, s, err.Error())
+				} else {
+					require.True(t, msg.(*regexp.Regexp).FindString(err.Error()) != ``)
+				}
+			} else {
+				panic(r)
+			}
+		}
+	}()
+	f()
+	require.Fail(t, `expected panic didn't happen`)
 }
 
 func TestParse_valueParamRef(t *testing.T) {
@@ -337,20 +358,4 @@ func TestParse_valueParamRef(t *testing.T) {
   }
 )`, buf.String())
 	})
-}
-
-func requireError(t *testing.T, msg string, f func()) {
-	t.Helper()
-	defer func() {
-		t.Helper()
-		if r := recover(); r != nil {
-			if err, ok := r.(error); ok {
-				require.Equal(t, msg, err.Error())
-			} else {
-				panic(r)
-			}
-		}
-	}()
-	f()
-	require.Fail(t, `expected panic didn't happen`)
 }
